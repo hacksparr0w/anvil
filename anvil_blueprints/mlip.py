2ec5efbb15a88e4c64d0c102920d04a954787510
hacksparr0w <hacksparr0w@protonmail.com>
2022-08-08 12:40:39 +0200

    feat: mlip blueprint

---
+++ anvil_blueprints/mlip.py
@@ -0,0 +1,48 @@
+import os
+
+from typing import List
+
+from anvil import Blueprint, GitSource, Source, run
+
+
+class Mlip(Blueprint):
+    @classmethod
+    def name(cls) -> str:
+        return "mlip"
+
+    @classmethod
+    def source(cls) -> Source:
+        return GitSource(
+            "git@gitlab.com:ashapeev/mlip-2.git"
+        )
+
+    @classmethod
+    def dependencies(cls) -> List[str]:
+        return []
+
+    def build(self) -> None:
+        paths = self.paths
+        build_directory = paths.current_package_build_directory
+        working_directory = paths.current_package_directory
+        environment = {"PATH": os.environ["PATH"]}
+
+        run(
+            ["./configure", "--enable-debug", f"--prefix={build_directory}"],
+            environment=environment,
+            working_directory=working_directory
+        )
+
+        run(
+            ["make", "mlp"],
+            environment=environment,
+            working_directory=working_directory
+        )
+
+        run(
+            ["make", "libinterface"],
+            environment=environment,
+            working_directory=working_directory
+        )
+
+    def is_built(self) -> bool:
+        return self.paths.current_package_build_directory.is_dir()
