cf30feef138008e32dbfcff57a5d5d6ef12f8b5c
hacksparr0w <hacksparr0w@protonmail.com>
2022-08-16 12:08:12 +0200

    feat: untested atomsk blueprint

---
+++ anvil_blueprints/atomsk.py
@@ -0,0 +1,44 @@
+import os
+
+from typing import List
+
+from anvil import Blueprint, GitSource, run
+
+
+class Atomsk(Blueprint):
+    @classmethod
+    def name(cls) -> str:
+        return "atomsk"
+
+    @classmethod
+    def source(cls) -> GitSource:
+        return GitSource("https://github.com/pierrehirel/atomsk.git")
+
+    @classmethod
+    def dependencies(cls) -> List[str]:
+        return []
+
+    def build(self) -> None:
+        paths = self.paths
+        build_directory = paths.current_package_build_directory
+        working_directory = paths.current_package_directory / "src"
+
+        environment = {
+            "PATH": os.environ["PATH"],
+            "INSTPATH": str(build_directory)
+        }
+
+        run(
+            ["make", "atomsk"],
+            environment=environment,
+            working_directory=working_directory
+        )
+
+        run(
+            ["make", "install"],
+            environment=environment,
+            working_directory=working_directory
+        )
+
+    def is_built(self) -> bool:
+        return self.paths.current_package_build_directory.is_dir()
